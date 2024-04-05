4d6850a4269e38769738bd413428e27de1eaff7e
Sergiom8m <sergiomartinportillo8@gmail.com>
2024-04-05 11:04:15 +0200

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,10 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
     username VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255)
 );
 
 CREATE TABLE activities (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT
     name VARCHAR(255),
     distance FLOAT,
     init_point VARCHAR(255),
